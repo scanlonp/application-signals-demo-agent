5312ee3be5a26a9cafc62b956c49894f3ed00d62
Haresh Nasit <hnnasit@amazon.com>
2025-04-21 20:32:34 -0700

    use 2 azs instead of 3

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -16,10 +16,10 @@ module "vpc" {
   name = var.cluster_name
   cidr = "10.0.0.0/16"
 
-  azs             = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1], data.aws_availability_zones.available.names[2]]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  database_subnets = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
+  azs             = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  database_subnets = ["10.0.201.0/24", "10.0.202.0/24"]
 
   enable_nat_gateway     = true
   single_nat_gateway     = true
