44ed58fcb3e0333258a4a999672c64f7f6c13bc7
Yong Tao <taoyong@amazon.com>
2025-04-22 14:53:27 -0700

    push commit sha to ecr

--- push-ecr.sh
+++ push-ecr.sh
@@ -7,36 +7,43 @@ aws ecr create-repository --repository-name springcommunity/spring-petclinic-api
 docker tag springcommunity/spring-petclinic-api-gateway:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-api-gateway:latest
 docker tag springcommunity/spring-petclinic-api-gateway:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-api-gateway:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-api-gateway:latest
+docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-api-gateway:${COMMIT_SHA}
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-discovery-server --region ${REGION} --no-cli-pager || true
 docker tag springcommunity/spring-petclinic-discovery-server:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-discovery-server:latest
 docker tag springcommunity/spring-petclinic-discovery-server:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-discovery-server:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-discovery-server:latest
+docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-discovery-server:${COMMIT_SHA}
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-config-server --region ${REGION} --no-cli-pager || true
 docker tag springcommunity/spring-petclinic-config-server:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-config-server:latest
 docker tag springcommunity/spring-petclinic-config-server:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-config-server:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-config-server:latest
+docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-config-server:${COMMIT_SHA}
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-visits-service --region ${REGION} --no-cli-pager || true
 docker tag springcommunity/spring-petclinic-visits-service:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-visits-service:latest
 docker tag springcommunity/spring-petclinic-visits-service:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-visits-service:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-visits-service:latest
+docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-visits-service:${COMMIT_SHA}
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-vets-service --region ${REGION} --no-cli-pager || true
 docker tag springcommunity/spring-petclinic-vets-service:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-vets-service:latest
 docker tag springcommunity/spring-petclinic-vets-service:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-vets-service:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-vets-service:latest
+docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-vets-service:${COMMIT_SHA}
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-customers-service --region ${REGION} --no-cli-pager || true
 docker tag springcommunity/spring-petclinic-customers-service:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-customers-service:latest
 docker tag springcommunity/spring-petclinic-customers-service:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-customers-service:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-customers-service:latest
+docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-customers-service:${COMMIT_SHA}
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-admin-server --region ${REGION} --no-cli-pager || true
 docker tag springcommunity/spring-petclinic-admin-server:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-admin-server:latest
 docker tag springcommunity/spring-petclinic-admin-server:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-admin-server:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-admin-server:latest
+docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-admin-server:${COMMIT_SHA}
 
 
 aws ecr create-repository --repository-name python-petclinic-insurance-service --region ${REGION} --no-cli-pager || true
@@ -44,6 +51,7 @@ docker build -t insurance-service ./pet_clinic_insurance_service --no-cache
 docker tag insurance-service:latest ${REPOSITORY_PREFIX}/python-petclinic-insurance-service:latest
 docker tag insurance-service:latest ${REPOSITORY_PREFIX}/python-petclinic-insurance-service:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/python-petclinic-insurance-service:latest
+docker push ${REPOSITORY_PREFIX}/python-petclinic-insurance-service:${COMMIT_SHA}
 
 
 aws ecr create-repository --repository-name python-petclinic-billing-service --region ${REGION} --no-cli-pager || true
@@ -51,24 +59,28 @@ docker build -t billing-service ./pet_clinic_billing_service --no-cache
 docker tag billing-service:latest ${REPOSITORY_PREFIX}/python-petclinic-billing-service:latest
 docker tag billing-service:latest ${REPOSITORY_PREFIX}/python-petclinic-billing-service:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/python-petclinic-billing-service:latest
+docker push ${REPOSITORY_PREFIX}/python-petclinic-billing-service:${COMMIT_SHA}
 
 aws ecr create-repository --repository-name nodejs-petclinic-nutrition-service --region ${REGION} --no-cli-pager || true
 docker build -t nutrition-service ./pet-nutrition-service --no-cache
 docker tag nutrition-service:latest ${REPOSITORY_PREFIX}/nodejs-petclinic-nutrition-service:latest
 docker tag nutrition-service:latest ${REPOSITORY_PREFIX}/nodejs-petclinic-nutrition-service:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/nodejs-petclinic-nutrition-service:latest
+docker push ${REPOSITORY_PREFIX}/nodejs-petclinic-nutrition-service:${COMMIT_SHA}
 
 aws ecr create-repository --repository-name traffic-generator --region ${REGION} --no-cli-pager || true
 docker build -t traffic-generator ./traffic-generator --no-cache
 docker tag traffic-generator:latest ${REPOSITORY_PREFIX}/traffic-generator:latest
 docker tag traffic-generator:latest ${REPOSITORY_PREFIX}/traffic-generator:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/traffic-generator:latest
+docker push ${REPOSITORY_PREFIX}/traffic-generator:${COMMIT_SHA}
 
 aws ecr create-repository --repository-name dotnet-petclinic-payment --region ${REGION} --no-cli-pager || true
 docker build -t dotnet-petclinic-payment ./dotnet-petclinic-payment/PetClinic.PaymentService --no-cache
 docker tag dotnet-petclinic-payment:latest ${REPOSITORY_PREFIX}/dotnet-petclinic-payment:latest
 docker tag dotnet-petclinic-payment:latest ${REPOSITORY_PREFIX}/dotnet-petclinic-payment:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/dotnet-petclinic-payment:latest
+docker push ${REPOSITORY_PREFIX}/dotnet-petclinic-payment:${COMMIT_SHA}
 
 # Create ECR repository
 aws ecr create-repository --repository-name otel-collector --region ${REGION} --no-cli-pager || true
@@ -98,3 +110,5 @@ docker build --build-arg REGION=${REGION} -t otel-collector -f ./otel-collector/
 docker tag otel-collector:latest ${REPOSITORY_PREFIX}/otel-collector:latest
 docker tag otel-collector:latest ${REPOSITORY_PREFIX}/otel-collector:${COMMIT_SHA}
 docker push ${REPOSITORY_PREFIX}/otel-collector:latest
+docker push ${REPOSITORY_PREFIX}/otel-collector:${COMMIT_SHA}
+
